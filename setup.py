5b330f0000086ee0d7534ad7bf9d40b7f3b25ceb
hecdelatorre <hector982015@gmail.com>
2024-02-03 18:28:52 -0600

    R

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pysqlitecrypto-rsa',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
